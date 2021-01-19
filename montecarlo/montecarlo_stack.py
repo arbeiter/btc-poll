ee6ed8f2fd82beb0c4bb7baa8e8174483957bac5
Siddharth Chauhan <csiddha@amazon.com>
2021-01-19 13:55:41 -0800

    Add api gateway setup

--- montecarlo/montecarlo_stack.py
+++ montecarlo/montecarlo_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
         core,
         aws_lambda as _lambda,
-        aws_apigateway as apigw
+        aws_apigateway as apigw,
+        aws_dynamodb as ddb,
     )
 
 
@@ -10,13 +11,22 @@ class MontecarloStack(core.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # Defines an AWS Lambda resource
-        my_lambda = _lambda.Function(
-            self, 'HelloHandler',
+        base_lambda = _lambda.Function(
+            self, 'BaseLambda',
             runtime=_lambda.Runtime.PYTHON_3_7,
             code=_lambda.Code.asset('lambda'),
-            handler='hello.handler',
+            handler='lambda-handler.handler',
         )
-        apigw.LambdaRestApi(
-            self, 'Endpoint',
-            handler=my_lambda
-            )
+        base_api = apigw.LambdaRestApi(
+            self, 'MonteCarloApi',
+            handler=base_lambda,
+            rest_api_name='ApiGatewayWithCors')
+
+        items = base_api.root.add_resource("metrics")
+        items.add_method("GET")
+        item = items.add_resource("{id}")
+        item.add_method("GET")
+
+        rank = base_api.root.add_resource("rank")
+        rank_fetch = rank.add_resource("{id}")
+        rank_fetch.add_method("GET")
