52448160589408d3a058e5109a651b62104757a3
Siddharth Chauhan <csiddha@amazon.com>
2021-01-18 20:49:12 -0800

    First commit

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from montecarlo.montecarlo_stack import MontecarloStack
+
+
+app = core.App()
+MontecarloStack(app, "montecarlo", env={'region': 'us-west-2'})
+
+app.synth()
