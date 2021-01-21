712f71c98ab563e78f333dba7765908e22714281
Siddharth Chauhan <csiddha@amazon.com>
2021-01-20 23:58:03 -0800

    Update dependencies, dynamodb logic

--- lambda/producer/lambda_function.py
+++ lambda/producer/lambda_function.py
@@ -1,22 +1,10 @@
 from __future__ import print_function
 import json
-import requests
+import urllib3
 import uuid
 import decimal
 import os
 import boto3
-
-
-# Helper class to convert a DynamoDB item to JSON.
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, decimal.Decimal):
-            if o % 1 > 0:
-                return float(o)
-            else:
-                return int(o)
-        return super(DecimalEncoder, self).default(o)
-
 
 # Get the service resource.
 dynamodb = boto3.resource('dynamodb')
@@ -27,13 +15,27 @@ coins = ["btcusd", "ltcusd", "dogeusd"]
 
 def fetch_coin_price(coin_name):
     url = f"https://api.cryptowat.ch/markets/kraken/{coin_name}/price"
-    r = requests.get('url')
-    return r.json()['result']['price']
+    http = urllib3.PoolManager()
+    r = http.request('GET', url)
+    price = json.loads(r.data)['result']['price']
+    return price
 
 def write_to_ddb(coin_name, coin_price):
-    pass
+    table = dynamodb.Table(TABLE_NAME)
+    response = table.update_item(
+        Key={id: coin_name},
+        UpdateExpression='set #prices = list_append(if_not_exists(#prices, :empty_list), :price)',
+        ExpressionAttributeNames={
+          '#prices': 'prices'
+        },
+        ExpressionAttributeValues={
+          ':price': [str(coin_price)],
+          ':empty_list': []
+        },
+        ReturnValues='ALL_NEW')
+    print(response)
 
 def lambda_handler(event, context):
-    table = dynamodb.Table(TABLE_NAME)
     for coin in coins:
-        fetch_coin_price(coin)
+        price = fetch_coin_price(coin)
+        write_to_ddb(coin, price)
